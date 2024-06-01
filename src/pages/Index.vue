e6bde34c74563d0694e3f008e89730ca35848362
dzackyluc <dzackylucifer12345@gmail.com>
2024-06-01 22:07:45 +0700

    Landing Done

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -15,7 +15,7 @@
         </div>
         <div class="grid grid-cols-6 col-span-3 gap-4 pr-20 pt-4">
             <div class="col-span-3 pt-9">
-                <div class="text-2xl">Depok</div>
+                <a class="text-2xl">Depok</a>
             </div>
             <div class="col-span-3 pt-9">
                 <div class="text-2xl underline text-end">Pilih Lokasi</div>
@@ -43,6 +43,78 @@
             <div class="bg-white rounded-lg grid grid-cols-2 col-span-6 shadow-lg p-4">
                 <div class="text-gray-800 font-bold col-span-1 text-2xl m-3">Isya</div>
                 <div class="text-gray-700 col-span-1 text-end text-xl m-3">07:30 PM</div>
+            </div>
+        </div>
+    </div>
+    <div class="container bg-red-100">
+        <div class="container">
+            <div class="text-4xl font-bold pt-12 pb-9 text-green-800 pl-16">Blog</div>
+            <div class="grid grid-cols-12 gap-5 pl-16 pb-12">
+                <div class="col-span-4">
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+                        <img class="w-full" src="https://smpn2dukuhturi.sch.id/wp-content/uploads/2022/08/Andhila-3.png" alt="Sunset in the mountains">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">Tata Cara Sholat</div>
+                            <p class="text-gray-700 text-base">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                            </p>
+                        </div>
+                        <div class="px-8 pt-4 pb-5">
+                            <button class="bg-green-800 hover:bg-gray-400 hover:text-black text-white rounded-xl py-2 px-28">Baca Disini</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-span-4">
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+                        <img class="w-full" src="https://smpn2dukuhturi.sch.id/wp-content/uploads/2022/08/Andhila-3.png" alt="Sunset in the mountains">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">Tata Cara Sholat</div>
+                            <p class="text-gray-700 text-base">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                            </p>
+                        </div>
+                        <div class="px-8 pt-4 pb-5">
+                            <button class="bg-green-800 hover:bg-gray-400 hover:text-black text-white rounded-xl py-2 px-28">Baca Disini</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-span-4">
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+                        <img class="w-full" src="https://smpn2dukuhturi.sch.id/wp-content/uploads/2022/08/Andhila-3.png" alt="Sunset in the mountains">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">Tata Cara Sholat</div>
+                            <p class="text-gray-700 text-base">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                            </p>
+                        </div>
+                        <div class="px-8 pt-4 pb-5">
+                            <button class="bg-green-800 hover:bg-gray-400 hover:text-black text-white rounded-xl py-2 px-28">Baca Disini</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="text-4xl font-bold pt-12 pb-9 text-green-800 pl-16">Kegiatan</div>
+        <div class="grid grid-cols-2 grid-rows-3 gap-4">
+            <div class="row-span-3 col-span-1">
+                <div class="flex p-16 rounded-xl bg-red-100">
+                    <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('https://img.pikbest.com/origin/09/01/22/25vpIkbEsTb7c.png!sw800')" title="Woman holding a mug"></div>
+                    <div class="rounded-b p-4 flex flex-col justify-between leading-normal">
+                        <div class="mb-5">
+                        <div class="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?</div>
+                        <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row-span-1 col-span-1">
+                <div class="p-6 rounded-xl bg-red-100">
+                    <div>
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
