f961e31d2c44e2150260cf3161a6586bc9972de9
KIGAMEKUN <reksa.prayoga1012@gmail.com>
2024-06-02 20:29:28 +0700

    feat(add dashboard)

--- src/pages/Admin/AdminDashboard.vue
+++ src/pages/Admin/AdminDashboard.vue
@@ -29,7 +29,7 @@
               <div class="card">
                 <div class="card-body px-4 py-4-5">
                   <div class="row">
-                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                    <div class="col-md-8 col-lg-12">
                       <h6 class="text-muted font-semibold">
                         <i class="bi bi-cash"></i> Infaq
                       </h6>
@@ -45,7 +45,7 @@
               <div class="card">
                 <div class="card-body px-4 py-4-5">
                   <div class="row">
-                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                    <div class="col-md-8 col-lg-12">
                       <h6 class="text-muted font-semibold">
                         <i class="bi bi-cash"></i> Zakat
                       </h6>
