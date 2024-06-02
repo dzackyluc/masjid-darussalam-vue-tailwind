d23b2740608e879ce4e304096a3573cff3481455
dzackyluc <dzackylucifer12345@gmail.com>
2024-06-02 13:23:14 +0700

    seven iteration (router programmatic error)

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -11,18 +11,18 @@ import Toolbar from 'primevue/toolbar';
         <template #end>
             <button :class="'font-light text-green-700 ' + ($route.path === '/' ? ' underline' : 'hover:underline')" @click="router.push('/');">Home</button>
             <button :class="'font-light text-green-700 px-4 ' + ($route.path === '/profil' ? ' underline' : 'hover:underline')" @click="router.push('/profil');">Profil</button>
-            <button :class="'font-light text-green-700 ' + ($route.path === '/galeri' ? ' underline' : 'hover:underline')" @click="this.$router.push('/galeri');">Galeri</button>
+            <button :class="'font-light text-green-700 ' + ($route.path === '/galeri' ? ' underline' : 'hover:underline')" @click="$router.push('/galeri');">Galeri</button>
             <div class="relative" @mouseover="isOpen = true" @mouseleave="isOpen = false">
                 <button class="font-light text-green-700 px-4 hover:underline">
                     Layanan <i class="pi pi-chevron-down"></i>
                 </button>
                 <div v-show="isOpen" class="absolute bg-green-800 border border-gray-300 rounded shadow-md z-50" @mouseover="isOpen = true" @mouseleave="isOpen = false">
-                    <button class="block w-full px-3 py-2 text-sm text-center text-white hover:bg-gray-300 hover:text-black" @click="this.$router.push('/infaq');">Infaq</button>
-                    <button class="block w-full px-3 py-2 text-sm text-center text-white hover:bg-gray-300 hover:text-black" @click="this.$router.push('/zakat');">Zakat</button>
-                    <button class="block w-full px-3 py-2 text-sm text-center text-white hover:bg-gray-300 hover:text-black" @click="this.$router.push('/reservasi');">Reservasi</button>
+                    <button class="block w-full px-3 py-2 text-sm text-center text-white hover:bg-gray-300 hover:text-black" @click="$router.push('/infaq');">Infaq</button>
+                    <button class="block w-full px-3 py-2 text-sm text-center text-white hover:bg-gray-300 hover:text-black" @click="$router.push('/zakat');">Zakat</button>
+                    <button class="block w-full px-3 py-2 text-sm text-center text-white hover:bg-gray-300 hover:text-black" @click="$router.push('/reservasi');">Reservasi</button>
                 </div>
             </div>
-            <button class="font-light text-green-700 px-4 py-3 hover:bg-gray-200 2xl:rounded-full" @click="this.$router.push('/login');">
+            <button class="font-light text-green-700 px-4 py-3 hover:bg-gray-200 2xl:rounded-full" @click="$router.push('/login');">
                 <i class="pi pi-user text-2xl"></i>
             </button>
         </template>
