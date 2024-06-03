e046e35b454bfe9b2a8cd3a11d79830e1625d12d
zulfanazka <167940167+zulfanazka@users.noreply.github.com>
2024-06-03 20:23:16 +0700

    Update AppFooter.vue

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,44 @@
+<template>
+    <footer style="background: #4E6F52">
+    <div class="max-w-screen-xl px-4 py-16 mx-auto space-y-8 sm:px-6 lg:space-y-2 lg:px-1">
+        <div class="grid grid-cols-1 gap-10 lg:grid-cols-4">
+            <div class="p-8 rounded-lg square-background" style="background: #E7DFCA;">
+                <div class="text-teal-600">
+                    <img class="h-12" src="C:\Users\zulfa\masjid-darussalam-vue-tailwind\src\assets\logo-appbar.png" alt="Logo">
+                </div>
+            
+                <p class="max-w-xs mt-4" style="color: #4E6F52; text-align:left">
+                    Jl. Komp. Bukit Cengkeh Berbunga No. Raya, Bakti Jaya, Kec. Sukmajaya, Kota Depok, Jawa Barat 16418
+                </p>
+            </div>
+            <div class="grid grid-cols-1 gap-8 mt-10 ml-5 sm:grid-cols-2 lg:col-span-3 lg:grid-cols-6">
+                <div>
+                    <a href="#" class="font-medium text-white hover:text-teal-600">Home</a>
+                </div>
+                <div>
+                    <a href="#" class="font-medium text-white hover:text-teal-600">Profile</a>
+                </div>
+                <div>
+                    <a href="#" class="font-medium text-white hover:text-teal-600">Galeri</a>
+                </div>
+                <div>
+                    <a href="#" class="font-medium text-white hover:text-teal-600">Zakat</a>
+                </div>
+                <div>
+                    <a href="#" class="font-medium text-white hover:text-teal-600">Infaq</a>
+                </div>
+                <div>
+                    <a href="#" class="font-medium text-white hover:text-teal-600">Reservasi</a>
+                </div>
+                <div class="grid grid-cols-1 gap-8 sm:grid-cols-3 lg:col-span-1 lg:grid-cols-6"></div>
+                <div class="flex">
+                    <i class="text-white bi bi-telephone"></i>
+                    <p class="ml-2 mr-20 font-medium text-white">081234567890</p>
+                    <i class="text-white bi bi-envelope"></i>
+                    <p class="ml-2 font-medium text-white">masjidjamidarusslam@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</template>
